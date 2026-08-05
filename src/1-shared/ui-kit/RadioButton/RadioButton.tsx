8342e1c311e2699828375732e19f9e1f75e2d3fa
wmrsmile2018 <wmrsmile2018@gmail.com>
2026-08-05 11:09:08 +0300

    removed memo for ui-kit

--- src/1-shared/ui-kit/RadioButton/RadioButton.tsx
+++ src/1-shared/ui-kit/RadioButton/RadioButton.tsx
@@ -10,25 +10,28 @@ type RedioButtonProps = {
   title?: string;
 };
 
-export const RedioButton: FC<RedioButtonProps> = memo(
-  ({ value, onChange, disabled, variant = 'circle' }) => {
-    const [checked, toggle] = useReducer((state) => !state, value ?? false);
+export const RedioButton: FC<RedioButtonProps> = ({
+  value,
+  onChange,
+  disabled,
+  variant = 'circle',
+}) => {
+  const [checked, toggle] = useReducer((state) => !state, value ?? false);
 
-    const onClick = () => {
-      if (disabled !== true) {
-        toggle();
-        onChange?.(!checked);
-      }
-    };
+  const onClick = () => {
+    if (disabled !== true) {
+      toggle();
+      onChange?.(!checked);
+    }
+  };
 
-    return (
-      <div
-        onClick={onClick}
-        className={`${styles.container} ${variant === 'circle' ? styles.circle : styles.rect} ${checked ? 'checked' : ''}`}
-      >
-        {variant === 'rect' && checked && <ConfirmIcon />}
-        {variant === 'circle' && checked && <div className={styles.dot} />}
-      </div>
-    );
-  }
-);
+  return (
+    <div
+      onClick={onClick}
+      className={`${styles.container} ${variant === 'circle' ? styles.circle : styles.rect} ${checked ? 'checked' : ''}`}
+    >
+      {variant === 'rect' && checked && <ConfirmIcon />}
+      {variant === 'circle' && checked && <div className={styles.dot} />}
+    </div>
+  );
+};
