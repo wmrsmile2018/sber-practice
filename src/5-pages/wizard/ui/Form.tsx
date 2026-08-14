999cb4ea0ed1c4a5dc05f6deab214b0536bbf45d
wmrsmile2018 <wmrsmile2018@gmail.com>
2026-08-14 21:20:54 +0300

    lesson-4 attempt-3

---
+++ src/5-pages/wizard/ui/Form.tsx
@@ -0,0 +1,5 @@
+import { SubscriptionWizard } from 'features/SubscriptionWizard';
+
+export const Form = () => {
+  return <SubscriptionWizard />;
+};
--- src/6-app/App.tsx
+++ src/6-app/App.tsx
@@ -2,12 +2,14 @@ import styles from './App.module.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { RegistrationPage } from 'pages/users';
+import { Form } from 'pages/wizard';
 function App() {
   return (
     <div className={styles.container}>
       <Provider store={store}>
         <RegistrationPage />
         {/* <TaskPage /> */}
+        <Form />
       </Provider>
     </div>
   );
